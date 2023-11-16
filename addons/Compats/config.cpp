751124868cd6ebdaca19bc48604c08732ed80d09
DartRuffian <skulldude327@gmail.com>
2023-11-16 09:00:14 -0600

    Main compat addon

---
+++ addons/Compats/config.cpp
@@ -0,0 +1,18 @@
+#include "script_component.hpp"
+
+
+class CfgPatches
+{
+    class ADDON
+    {
+        author = "DartRuffian";
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] =
+        {
+            "A3_Data_F_Decade_Loadorder",
+            "MDF_Main"
+        };
+        units[] = {};
+        weapons[] = {};
+    };
+};
