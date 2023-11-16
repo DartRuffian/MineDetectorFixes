6d20e6151412977b2e73825526bf83d0a8ccf4c9
DartRuffian <skulldude327@gmail.com>
2023-11-16 16:29:41 -0600

    Macro for simple fixes

--- addons/Compats/JLTS/CfgWeapons.hpp
+++ addons/Compats/JLTS/CfgWeapons.hpp
@@ -2,27 +2,6 @@ class CfgWeapons
 {
     class CBA_MiscItem;
     class CBA_MiscItem_ItemInfo;
-    class JLTS_credits_10: CBA_MiscItem
-    {
-        detectRange = 0;
-        simulation = "Weapon";
-        type = TYPE_ITEM;
-
-        class ItemInfo: CBA_MiscItem_ItemInfo
-        {
-            type = TYPE_BIPOD;
-        };
-    };
-
-    class JLTS_droidCaller: CBA_MiscItem
-    {
-        detectRange = 0;
-        simulation = "Weapon";
-        type = TYPE_ITEM;
-
-        class ItemInfo: CBA_MiscItem_ItemInfo
-        {
-            type = TYPE_BIPOD;
-        };
-    };
+    SIMPLE_PATCH(JLTS_credits_10);
+    SIMPLE_PATCH(JLTS_droidCaller);
 };
