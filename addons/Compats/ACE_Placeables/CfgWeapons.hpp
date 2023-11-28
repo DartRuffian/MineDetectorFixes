3b16a1ff766d213fed83a856f2bd953249c22680
DartRuffian <skulldude327@gmail.com>
2023-11-27 19:39:07 -0600

    ACE Placeables Patch

---
+++ addons/Compats/ACE_Placeables/CfgWeapons.hpp
@@ -0,0 +1,6 @@
+class CfgWeapons
+{
+    class CBA_MiscItem;
+    class CBA_MiscItem_ItemInfo;
+    SIMPLE_PATCH(kka3_itemcore);
+};
---
+++ addons/Compats/ACE_Placeables/script_component.hpp
@@ -0,0 +1,4 @@
+#define SUBCOMPONENT ACE_PLACEABLES
+#define SUBCOMPONENT_BEAUTIFIED ACE - Placeables
+
+#include "..\script_component.hpp"
