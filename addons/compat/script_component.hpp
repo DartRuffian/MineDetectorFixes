c532e312a97924e6b490f5468ed5e48cf113a34a
DartRuffian <skulldude327@gmail.com>
2024-07-25 22:29:28 -0500

    compats -> compat

--- addons/compat/script_component.hpp
+++ addons/compat/script_component.hpp
@@ -1,5 +1,5 @@
-#define COMPONENT compats
-#define COMPONENT_BEAUTIFIED Compatibility Patches
+#define COMPONENT compat
+#define COMPONENT_BEAUTIFIED Compatibility Patch
 
 #include "\DA\mdf\addons\main\script_mod.hpp"
 #include "\DA\mdf\addons\main\script_macros.hpp"
