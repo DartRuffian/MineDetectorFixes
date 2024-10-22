589eacb8fd4a28aa33a4d3834bb6c95427b0c926
DartRuffian <skulldude327@gmail.com>
2024-10-22 13:27:21 -0500

    Formatting

--- addons/compat/script_macros.hpp
+++ addons/compat/script_macros.hpp
@@ -1,12 +1,13 @@
-#define SIMPLE_PATCH(className,baseClass,itemInfoBaseClass) class className: baseClass { \
-    type = TYPE_ITEM; \
-    simulation = "Weapon"; \
-    detectRange = 0; \
-    ace_asItem = 1; \
-    class ItemInfo: itemInfoBaseClass { \
-        type = TYPE_BIPOD; \
-    }; \
-}
+#define SIMPLE_PATCH(className,baseClass,itemInfoBaseClass) \
+    class className: baseClass { \
+        type = TYPE_ITEM; \
+        simulation = "Weapon"; \
+        detectRange = 0; \
+        ace_asItem = 1; \
+        class ItemInfo: itemInfoBaseClass { \
+            type = TYPE_BIPOD; \
+        }; \
+    }
 
 // Mods tend to use either InventoryWeapon_Base_F or InventoryItem_Base_F for a base class for ItemInfo.
 // InventoryWeapon_Base_F seemed to be more common for the mods I tested.
