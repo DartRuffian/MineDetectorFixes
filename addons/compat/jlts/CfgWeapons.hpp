5f552c041cad280ba5dc62273f2fa38a1b1005a5
DartRuffian <skulldude327@gmail.com>
2024-11-03 11:20:55 -0600

    Fix base class for droid caller

--- addons/compat/jlts/CfgWeapons.hpp
+++ addons/compat/jlts/CfgWeapons.hpp
@@ -1,7 +1,8 @@
 class CfgWeapons {
     class ItemCore;
+    class InventoryItem_Base_F;
     class InventoryWeapon_Base_F;
     SIMPLE_PATCH_VANILLA(JLTS_repairkit_weapon);
     SIMPLE_PATCH_VANILLA(JLTS_riot_shield_item);
-    SIMPLE_PATCH_VANILLA(JLTS_droidCaller);
+    SIMPLE_PATCH(JLTS_droidCaller,ItemCore,InventoryItem_Base_F);
 };
